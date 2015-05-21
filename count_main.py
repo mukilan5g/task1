7ee6f84dee223d101c9fe4b4674047a32a56a96e
mukilan5g <mukilan@5gindia.net>
2015-05-21 15:14:37 +0530

    After Testing

--- count_main.py
+++ count_main.py
@@ -1,7 +1,12 @@
 import argparse
 from sys import argv
+
 from count_lib import *
 
+#GETS the respsonce from url 
+#COLLECTS the contents
+#COUNT the frequency of the words
+#SORT the words in *descending* based on their frequency 
 def main(n):
 	words = collect_contents(url_responce().read().lower())
 	stop_words = read_stop_words()
@@ -10,8 +15,10 @@ def main(n):
 	for content in sorted_list[0:n]:
 		print '%s : %d'%content
 
-
+#DEFINING the named_argument *n* as an integer with default value 10
 parser = argparse.ArgumentParser()
 parser.add_argument('-n',dest='n',default=10,type=int)
 args = parser.parse_args()
+
+#CALL the main function with command_line_arguments
 main(args.n)
